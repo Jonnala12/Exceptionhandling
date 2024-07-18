e542709e092d78b1ee05f013a57c1b3db71c36dd
vamsh <saivivek1033@gmail.com>
2024-07-18 09:53:57 +0530

    Add ArrayAcces Class

---
+++ ArrayAcces.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class ArrayAcces {
+
+    private int[] numbers;
+
+    public ArrayAcces() {
+        this.numbers = new int[]{10, 20, 30, 40, 50};
+    }
+
+
+    public void accessElement() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter an index to access the element: ");
+            try {
+                int index = scanner.nextInt();
+                System.out.println("Element at index " + index + " is: " + numbers[index]);
+                break;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Invalid index! Please try again.");
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please enter a valid integer.");
+                scanner.next();
+            }
+        }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        ArrayAcces arrayAccess = new ArrayAcces();
+        arrayAccess.accessElement();
+    }
+}
+
